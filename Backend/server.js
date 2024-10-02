832d56afd03ec443899020e169a097cfc987068c
Devansh Kumar <devanshkumar6298@gmail.com>
2024-10-02 19:56:01 +0530

    3rd party middleware added

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,16 @@
 const express = require("express")
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const PORT = process.env.PORT || 3500
 
+app.use(logger)
+app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 app.use('/', express.static(path.join(__dirname, '/public')))
 app.use('/', require('./routes/root.js'))
 
@@ -17,6 +24,8 @@ app.all('*',(req,res)=>{
         res.type('txt').send('404 not found')
     }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
