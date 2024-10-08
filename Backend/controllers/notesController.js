705f97f8ef190315e0a285e26b1ab786a6e35e7d
Devansh Kumar <devanshkumar6298@gmail.com>
2024-10-08 21:40:17 +0530

    notes controller updated, reduced quries sent to database

--- Backend/controllers/notesController.js
+++ Backend/controllers/notesController.js
@@ -10,14 +10,45 @@ const getAllNotes = asyncHandler(async (req, res) => {
 
 
     if (!notes?.length) {
-        return res.status(404).json({ message: `there are no notes sooiciated` })
+        return res.status(404).json({ message: `no notes found` })
     }
-    return res.json(notes)
     
+    //here note.user means notes.id which refers to user.id i.e. notes.user = user.id
+    const notesWithUser = await Note.find().populate('user', 'username').lean().exec();
+
+    
+    
+    return res.json(notesWithUser)
 })
+
 const createNewNote = asyncHandler(async (req, res) => {
 
+    const {user, title, text} = req.body
+
+    //confirm the data
+    if(!user || !title || !text){
+        return res.status(400).json({message:"all ields are required"})
+    }
+
+    //check for duplicate title
+
+    const duplicateTitle = await Note.findOne({title}).lean().exec()
+    if (duplicateTitle) {
+        return res.status(409).json({ message: 'note title already exists' })
+    }
+
+    const notesObject = {user,title,text}
+
+    const note = await Note.create(notesObject)
+
+    if (note) {
+        res.status(201).json({ message: `New note ${note.title} created successfuly` })
+    } else {
+        res.status(400).json({ message: 'invalid note data recieved' })
+    }
+
 })
+
 const updateNote = asyncHandler(async (req, res) => {
 
 })
--- Backend/models/Note.js
+++ Backend/models/Note.js
@@ -5,7 +5,7 @@ const noteSchema = new mongoose.Schema(
     {
         user: {
             type: mongoose.Schema.Types.ObjectId,
-            required: true,
+            required: true, 
             ref: 'User'
         },
         title: {
