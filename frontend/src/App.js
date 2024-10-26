f99e839dad6b4bd50e87d2d549e16be14f47fb04
Devansh Kumar <devanshkumar6298@gmail.com>
2024-10-26 19:22:13 +0530

    RTK query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,30 +6,38 @@ import DashLayout from "./components/DashLayout";
 import Welcome from "./features/auth/Welcome";
 import NotesList from "./features/notes/NotesList";
 import UsersList from "./features/users/UsersList";
+import EditUser  from "./features/users/EditUser";
+import NewUserForm from "./features/users/NewUserForm";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features/notes/NewNote";
 
 
 
 function App() {
   return (
     <Routes>
-      <Route path="" element={<Layout />} />
+      <Route path="/" element={<Layout />} />
 
       {/*make sure to change the information inside */}
       <Route index element={<Public />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="login" element={<Login />} />
 
-      <Route path="/dash" element={<DashLayout />}>
+      <Route path="dash" element={<DashLayout />}>
 
         <Route index element={<Welcome />} />
 
-        <Route path="notes">
-        <Route index element={<NotesList/>}/> 
-        </Route>
-      
         <Route path="users">
         <Route index element={<UsersList/>}/> 
+        <Route path=":id" element={<EditUser/>}/>
+        <Route path=":new" element={<NewUserForm/>}/>
         </Route>
 
+        <Route path="notes">
+        <Route index element={<NotesList/>}/> 
+        <Route path=":id" element={<EditNote/>}/>
+        <Route path=":new" element={<NewNote/>}/>
+        </Route>
+      
 
       </Route>{/* End Of Dash path */}
     </Routes>
