a5815678a4036d0cf6d5631b6e6528a876b6d7e4
Devansh Kumar <devanshkumar6298@gmail.com>
2024-10-26 19:29:38 +0530

    updated Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,36 +10,38 @@ import EditUser  from "./features/users/EditUser";
 import NewUserForm from "./features/users/NewUserForm";
 import EditNote from "./features/notes/EditNote";
 import NewNote from "./features/notes/NewNote";
+import Prefetch from "./features/auth/Prefetch";
 
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
 
-      {/*make sure to change the information inside */}
-      <Route index element={<Public />} />
-      <Route path="login" element={<Login />} />
+        <Route element={<Prefetch />}>
+          <Route path="dash" element={<DashLayout />}>
 
-      <Route path="dash" element={<DashLayout />}>
+            <Route index element={<Welcome />} />
 
-        <Route index element={<Welcome />} />
+            <Route path="users">
+              <Route index element={<UsersList />} />
+              <Route path=":id" element={<EditUser />} />
+              <Route path="new" element={<NewUserForm />} />
+            </Route>
 
-        <Route path="users">
-        <Route index element={<UsersList/>}/> 
-        <Route path=":id" element={<EditUser/>}/>
-        <Route path=":new" element={<NewUserForm/>}/>
+            <Route path="notes">
+              <Route index element={<NotesList />} />
+              <Route path=":id" element={<EditNote />} />
+              <Route path="new" element={<NewNote />} />
+            </Route>
+
+          </Route>{/* End Dash */}
         </Route>
 
-        <Route path="notes">
-        <Route index element={<NotesList/>}/> 
-        <Route path=":id" element={<EditNote/>}/>
-        <Route path=":new" element={<NewNote/>}/>
-        </Route>
-      
-
-      </Route>{/* End Of Dash path */}
+      </Route>
     </Routes>
   );
 }
