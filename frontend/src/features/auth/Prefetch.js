c2db29ca1212fc214ee487a80a91579c60be8b31
Devansh Kumar <devanshkumar6298@gmail.com>
2024-10-28 19:50:15 +0530

    Refactor error handling to include isError flag in response

--- frontend/src/features/auth/Prefetch.js
+++ frontend/src/features/auth/Prefetch.js
@@ -5,16 +5,11 @@ import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Prefetch = () => {
+    
     useEffect(() => {
-        console.log('subscribing')
-        const notes = store.dispatch(notesApiSlice.endpoints.getNotes.initiate())
-        const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate())
+        store.dispatch(notesApiSlice.util.prefetch('getNotes','notesList',{force:true}))
+        store.dispatch(usersApiSlice.util.prefetch('getUsers','usersList',{force:true}))
 
-        return () => {
-            console.log('unsubscribing')
-            notes.unsubscribe()
-            users.unsubscribe()
-        }
     }, [])
 
     return <Outlet />
