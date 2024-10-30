57ecac380d9ac8a7ff4a6b197228ba5e37ab1f19
Devansh Kumar <devanshkumar6298@gmail.com>
2024-10-31 02:28:03 +0530

    Update link text in Welcome component for clarity

--- frontend/src/features/auth/Welcome.js
+++ frontend/src/features/auth/Welcome.js
@@ -19,9 +19,9 @@ const Welcome = () => {
 
             <h1>Welcome {username}!</h1>
 
-            <p><Link to="/dash/notes">View techNotes</Link></p>
+            <p><Link to="/dash/notes">View Tasks</Link></p>
 
-            <p><Link to="/dash/notes/new">Add New techNote</Link></p>
+            <p><Link to="/dash/notes/new">Add New Tasks</Link></p>
 
             {(isManager || isAdmin) && <p><Link to="/dash/users">View User Settings</Link></p>}
 
