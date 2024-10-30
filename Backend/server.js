9d3265387a8b6cb614c983418513d2af83320fab
Devansh Kumar <devanshkumar6298@gmail.com>
2024-10-31 01:33:17 +0530

    Update server configuration for production and change API base URL to deployed instance

--- Backend/server.js
+++ Backend/server.js
@@ -36,12 +36,6 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 not found')
     }
 })
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend/build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
-    })}
-
 app.use(errorHandler)
 
 mongoose.connection.once('open', () => {
