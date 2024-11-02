379a97f21713138883babf2064ff41f8c732714d
Devansh Kumar <devanshkumar6298@gmail.com>
2024-11-02 17:30:27 +0530

    Add loading spinner to DashLayout while content is being fetched

--- frontend/src/components/DashLayout.js
+++ frontend/src/components/DashLayout.js
@@ -4,6 +4,8 @@ import DashFooter from "./DashFooter"
 import { hideToast } from "../app/toastSlice";
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from "react";
+import PulseLoader from "react-spinners/PulseLoader";
+
 
 
 
@@ -15,11 +17,12 @@ const DashLayout = () => {
             setTimeout(() => { dispatch(hideToast()); }, 3000); // Hide toast after 3 seconds 
             } 
         }, [toast, dispatch]);
+        content = <Outlet/>? <Outlet/>:<PulseLoader color={"#FFF"} />
     return (
         <>
             <DashHeader />
             <div className="dash-container">
-                <Outlet />
+                {content}
                 {toast.visible && <div className="toast-thapa">{toast.message}</div>}
             </div>
             <DashFooter />
